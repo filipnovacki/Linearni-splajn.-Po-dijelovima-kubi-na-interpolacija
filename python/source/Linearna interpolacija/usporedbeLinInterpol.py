c3f77a5d4f1b576c92579eed242642effe685720
Filip Novački <otorinolaringolozi@gmail.com>
2018-05-02 23:58:02 +0200

    Uređen kod i dodan jednak aspect ratio osi

--- python/source/Linearna interpolacija/usporedbeLinInterpol.py
+++ python/source/Linearna interpolacija/usporedbeLinInterpol.py
@@ -3,19 +3,24 @@ import numpy as np
 
 plt.grid()
 ax = plt.subplot(111)
-const = 2*np.pi/40
-i=1
-while(i<=40):
+const = 2 * np.pi / 40
+i = 1
+while (i <= 40):
+    # Brise canvas tako da se moze crtati od pocetka
     ax.clear()
 
-    opseg = np.arange(-0.00001 + i*const, (i+1)*const, 2*np.pi/40)
+    # Aspect ratio osi je jednak
+    ax.set_aspect(aspect="equal")
+
+    # Racunanje linearne interpolacije
+    opseg = np.arange(-0.00001 + i * const, (i + 1) * const, 2 * np.pi / 40)
     vrijednosti = np.sin(opseg)
     plt.plot(opseg, vrijednosti)
 
-    opseg_detaljan = np.arange(-0.00001 + i*const, (i+1)*const, 0.0001)
+    # Racunanje kvaziprave funkcije
+    opseg_detaljan = np.arange(-0.00001 + i * const, (i + 1) * const, 0.0001)
     vrijednosti_detaljne = np.sin(opseg_detaljan)
     plt.plot(opseg_detaljan, vrijednosti_detaljne)
-    #plt.show()
 
-    plt.savefig('slike/usporedba'+str(i)+'.png')
-    i+=1
+    plt.savefig('slike/usporedba' + str(i) + '.png', dpi=150)
+    i += 1
