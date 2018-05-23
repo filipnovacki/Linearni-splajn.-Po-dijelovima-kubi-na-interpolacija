f1be521f37a813560d35a6bfd533b739972f5e69
Filip Novački <otorinolaringolozi@gmail.com>
2018-05-23 23:11:23 +0200

    Dodana tablica rješenja za 10. zadatak

--- python/source/tablice.py
+++ python/source/tablice.py
@@ -167,6 +167,7 @@ def kub_sin_table():
               format(c3, '.4f'), '\t',
               format(razlika, '.4f'))
 
+
 def kub_sin_latex():
     b = 2 * np.pi
     a = 0
@@ -193,9 +194,41 @@ def kub_sin_latex():
               format(razlika, '.4f'), "\\\\")
 
 
-def kubicni_prvi():
-    pass
+def kub_drugi_table():
+    print("c0\tc1\t\tc2\t\tc3\t\tRazlika")
+    tocka = 1/(1.2**2+1)
+    #print("Funkcija interpolacije\t\tRazlika f(1)-interpolacija")
+    print("............................................................................")
+    for k in range(-20,20):
+        x = 1 / 4
+        # Racunanje koeficijenata:
+        c0 = 1 / ((k * x) ** 2 + 1)
+        c1 = -(2 * (k * x)) / (((k * x) ** 2 + 1) ** 2)
+        c2 = (2 * (3 * (k * x) ** 2 - 1)) / (((k * x) ** 2 + 1) ** 3)
+        c3 = -(24 * k * x * ((k * x) ** 2 - 1)) / (((k * x) ** 2 + 1) ** 4)
 
+        razlika = tocka - (c0 + c1 * (1.2 - k * x) + c2 * (1.2 - k * x) ** 2 + c3 * (1.2 - k * x) ** 3)
+
+        #print(format(c0,'.3f')+format(c1, '.3f') if c1<0 else ("+" + format(c1,'.3f')) + '(x'+ str(x*(k-1)) if (x*(k-1)<0) else ('-'+str(x*(k-1))) + ')'+format(c2, '.3f') if c2<0 else ("+" + format(c2,'.3f')) + '(x'+ str(x*(k-1)) if x*(k-1)<0 else ('-'+str(x*(k-1))) + ')^2'+format(c3, '.3f') if c3 < 0 else ("+" + format(c3, '.3f')) + '(x' + str(x * (k - 1)) if x * (k - 1) < 0 else ('-' + str(x * (k - 1))) + ')^3')
+        print(format(c0, '.3f'), " \t", format(c1, '.3f'), " \t", format(c2, '.3f'), " \t", format(c3, '.3f'),"\t", razlika)
+
+
+def kub_drugi_latex():
+    print("c0&c1&c2&c3&Razlika")
+    tocka = 1 / (1.2 ** 2 + 1)
+    # print("Funkcija interpolacije\t\tRazlika f(1)-interpolacija")
+    for k in range(-20, 20):
+        x = 1 / 4
+        # Racunanje koeficijenata:
+        c0 = 1 / ((k * x) ** 2 + 1)
+        c1 = -(2 * (k * x)) / (((k * x) ** 2 + 1) ** 2)
+        c2 = (2 * (3 * (k * x) ** 2 - 1)) / (((k * x) ** 2 + 1) ** 3)
+        c3 = -(24 * k * x * ((k * x) ** 2 - 1)) / (((k * x) ** 2 + 1) ** 4)
+
+        razlika = tocka - (c0 + c1 * (1.2 - k * x) + c2 * (1.2 - k * x) ** 2 + c3 * (1.2 - k * x) ** 3)
+
+        print(format(c0, '.3f'), " &", format(c1, '.3f'), " &", format(c2, '.3f'), " &", format(c3, '.3f'), "&",
+              format(razlika, '.3f'),"\\\\")
 
 def meni(odabir):
     if odabir == 2:
@@ -217,8 +250,10 @@ def meni(odabir):
         kub_sin_latex()
         pass
     elif odabir == 7:
+        kub_drugi_table()
         pass
     elif odabir == 8:
+        kub_drugi_latex()
         pass
     elif odabir == 9:
         pass
